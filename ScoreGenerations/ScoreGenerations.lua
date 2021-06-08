8d0a1db8e214609247089a772f5316c7a607acf4
HyperBE32 <HyperPolygon64@gmail.com>
2021-06-08 20:57:58 +0100

    Readjusted rank tables to account for new time bonus

--- ScoreGenerations/ScoreGenerations.lua
+++ ScoreGenerations/ScoreGenerations.lua
@@ -1,21 +1,9 @@
 function calculateTimeBonus()
 	if (elapsedTime < minSeconds) then
-		for i = minSeconds - elapsedTime, 0, -1
-		do
-			-- For every second above the minimum required bonus time, add 10 points.
-			elapsedTime = (minSeconds - elapsedTime) + 10
-		end
-		
-		-- Multiply bonus by 10.
-		elapsedTime = elapsedTime * 10
-	elseif (elapsedTime < maxSeconds) then
-		for i = (maxSeconds - elapsedTime) / 10, 0, -1
-		do
-			-- For every 10 seconds above the maximum allowed bonus time, add 10 points.
-			elapsedTime = (maxSeconds - elapsedTime) + 10
-		end
+		elapsedTime = (minSeconds - elapsedTime) * 100
+	elseif (elapsedTime < maxSeconds) then	
+		elapsedTime = (maxSeconds - elapsedTime) * 10
 	else
-		-- Don't reward the player with a bonus.
 		elapsedTime = 0
 	end
 	
