8687cdac3ccb138bada5e7c51943130f53f96a2f
Hyper <HyperPolygon64@gmail.com>
2021-07-25 23:41:12 +0100

    [Sonic '06 HUD] Added brianuuu's fading hooks

--- Sonic Generations/Sonic06HUD/HudSonicStage.cpp
+++ Sonic Generations/Sonic06HUD/HudSonicStage.cpp
@@ -1,5 +1,32 @@
 int millisecondsDigit3 = 0;
 
+HOOK(float*, __fastcall, MsgFadeIn, 0x10CEC90, void* This, void* Edx, float* a2)
+{
+	a2[6] = ((float*)This)[24]; // start alpha
+
+	if (a2[6] > 0.0f)
+	{
+		a2[4] = 0.2f; // transition time
+	}
+
+	return originalMsgFadeIn(This, Edx, a2);
+}
+
+HOOK(float*, __fastcall, MsgFadeOut, 0x10CEDB0, void* This, void* Edx, float* a2)
+{
+	a2[4] = 0.0f; // transition time
+	a2[6] = 1.0f; // start alpha
+
+	return originalMsgFadeOut(This, Edx, a2);
+}
+
+HOOK(void, __stdcall, FadeOutCountDec, 0x10CE9A0)
+{
+	originalFadeOutCountDec();
+
+	(*(uint32_t**)0x1E66B40)[2] = 0;
+}
+
 /// <summary>
 /// Calculates a new set of milliseconds so it's compatible with three digits.
 /// </summary>
@@ -40,4 +67,13 @@ void HudSonicStage::Install()
 {
 	// Jump to milliseconds formatter to create triple digits.
 	WRITE_JUMP(0x1098D7A, &MillisecondsFormatter_MidAsmHook);
+
+	/**** brianuuu hooks ****/
+
+	// Install hook to lock player control during loading transitions.
+	INSTALL_HOOK(FadeOutCountDec);
+
+	// Install hooks to fix fading transitions.
+	INSTALL_HOOK(MsgFadeIn);
+	INSTALL_HOOK(MsgFadeOut);
 }
--- Sonic Generations/Sonic06HUD/Mod.cpp
+++ Sonic Generations/Sonic06HUD/Mod.cpp
@@ -3,9 +3,6 @@
 /// </summary>
 extern "C" _declspec(dllexport) void Init()
 {
-	// Lock player control during loading transitions.
-	WRITE_JUMP(0xD97EA4, (void*)0xD97EAF);
-
 	// Use single digit for life counter.
 	WRITE_MEMORY(0x1098CCA, char*, "%d");
 
