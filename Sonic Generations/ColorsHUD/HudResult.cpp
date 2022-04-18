316d3baa30072371d1e8a4cddcc8dc1a27e90393
Hyper <34012267+HyperBE32@users.noreply.github.com>
2022-04-18 23:01:53 +0100

    [ColorsHUD] Added custom results camera

--- Sonic Generations/ColorsHUD/HudResult.cpp
+++ Sonic Generations/ColorsHUD/HudResult.cpp
@@ -14,9 +14,26 @@ __declspec(naked) void ResultTimer_MidAsmHook()
 	}
 }
 
+// https://github.com/brianuuu/DllMods/blob/master/Source/Sonic06HUD/ResultUI.cpp#L38
+HOOK(int, __fastcall, CStateGoalFadeBefore, 0xCFE080, uint32_t* thisDeclaration)
+{
+	/* Adjust the results camera position so Sonic appears to
+	   be sitting on top of the bottom UI element like Colours. */
+
+	WRITE_MEMORY(0x1A48C7C, float, -0.2f);	 // X Position
+	WRITE_MEMORY(0x1A48C80, float, 1.25f);	 // Y Position
+	WRITE_MEMORY(0x1A48C84, float, 9);		 // Z Position
+	WRITE_MEMORY(0x1A48C88, float, 2);		 // X Rotation
+	WRITE_MEMORY(0x1A48C8C, float, -10.25f); // Y Rotation
+
+	return originalCStateGoalFadeBefore(thisDeclaration);
+}
+
 void HudResult::Install()
 {
 	// Do not modify the results timer if Customizable Results Music is enabled.
 	if (GetModuleHandle(TEXT("CustomizableResultsMusic.dll")) == nullptr)
 		WRITE_JUMP(0xCFD55E, &ResultTimer_MidAsmHook);
+
+	INSTALL_HOOK(CStateGoalFadeBefore);
 }
