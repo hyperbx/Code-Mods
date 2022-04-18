4042ca0b0199f6b455daada0c701e41414c5281c
Hyper <34012267+HyperBE32@users.noreply.github.com>
2022-04-18 23:02:05 +0100

    [ColorsHUD] Disable extended boost gauge

--- Sonic Generations/ColorsHUD/HudSonicStage.cpp
+++ Sonic Generations/ColorsHUD/HudSonicStage.cpp
@@ -1,4 +1,4 @@
-// https://github.com/PTKickass/ModUpdates/blob/master/DLLModsSource/SonicGenerations/SonicUnleashedHUD/UnleashedHUD/DataHelper.cpp#L87
+// https://github.com/brianuuu/DllMods/blob/master/Source/Sonic06HUD/Stage.cpp#L166
 HOOK(void, __fastcall, CHudSonicStageUpdate, 0x1098A50, void* thisDeclaration, void* edx, float* pUpdateInfo)
 {
 	// Always clamp boost to 100.
@@ -7,8 +7,22 @@ HOOK(void, __fastcall, CHudSonicStageUpdate, 0x1098A50, void* thisDeclaration, v
 	originalCHudSonicStageUpdate(thisDeclaration, edx, pUpdateInfo);
 }
 
+// https://github.com/brianuuu/DllMods/blob/master/Source/Sonic06HUD/Stage.cpp#L174
+HOOK(int, __fastcall, MsgRestartStage, 0xE76810, uint32_t* thisDeclaration, void* edx, void* message)
+{
+	int result = originalMsgRestartStage(thisDeclaration, edx, message);
+
+	// Force disable extended boost.
+	*(uint32_t*)((uint32_t)*BlueBlurCommon::PlayerContext + 0x680) = 1;
+
+	return result;
+}
+
 void HudSonicStage::Install()
 {
-	// Install hook to update the boost gauge.
+	// Install hook to cap the boost gauge to 100%.
 	INSTALL_HOOK(CHudSonicStageUpdate);
+
+	// Install hook to disable the extended boost gauge.
+	INSTALL_HOOK(MsgRestartStage);
 }
