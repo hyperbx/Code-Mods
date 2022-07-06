7b1fb90764f32e095daa2f2fd1d7ff215e128a4b
Hyper <34012267+HyperBE32@users.noreply.github.com>
2022-07-06 14:33:57 +0100

    [Score Generations] Save negative rank values as D rank

--- Sonic Generations/ScoreGenerations/Patches.cpp
+++ Sonic Generations/ScoreGenerations/Patches.cpp
@@ -1,12 +1,15 @@
 void Patches::Install()
 {
-	// Remove next rank dialog in results.
-	WRITE_JUMP(0x10B66F0, (void*)0x10B6700);
-	WRITE_JUMP(0x10B7DAE, (void*)0x10B7DB7);
+	// Skip score cap in Casino Night.
+	WRITE_JUMP(0x1095D70, (void*)0x1095D77);
 
 	// Skip score cap in results.
 	WRITE_JUMP(0x10B4012, (void*)0x10B401E);
 
-	// Skip score cap in Casino Night.
-	WRITE_JUMP(0x1095D70, (void*)0x1095D77);
+	// Remove next rank dialog in results.
+	WRITE_JUMP(0x10B66F0, (void*)0x10B6700);
+	WRITE_JUMP(0x10B7DAE, (void*)0x10B7DB7);
+
+	// Register negative rank values as D rank.
+	WRITE_MEMORY(0x10B92BE, uint8_t, 0x7D);
 }
