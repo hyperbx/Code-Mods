2def76a347f660636fc7d721c8bc0749c3626cea
Gabriel <34012267+HyperBE32@users.noreply.github.com>
2023-05-27 21:24:20 +0100

    Use CommonLoaderAPI for signature scanning

--- Dependencies/Signature.h
+++ Dependencies/Signature.h
@@ -5,38 +5,6 @@
 
 inline bool m_IsSigValid = true;
 
-#if CL_SCANNER
-
-#define CL_SCAN_SIGNATURE(x, ...) \
-FORCEINLINE void* x(); \
-inline void* x##Addr = x(); \
-FORCEINLINE void* x() \
-{ \
-    if (x##Addr) \
-        return x##Addr; \
-    CommonLoader_GetAPI()->SetState(CMN_LOADER_STATE_SKIP_SIG_VALIDATION, 1); \
-    x##Addr = CommonLoader_GetAPI()->ScanSignature(__VA_ARGS__); \
-    printf("[Signature] %s received: 0x%llx\n", #x, x##Addr); \
-    if (x##Addr == nullptr) \
-        m_IsSigValid = false; \
-    return x##Addr; \
-}
-
-#define CL_SCAN_SIGNATURE_ALLOW_NULL(x, ...) \
-FORCEINLINE void* x(); \
-inline void* x##Addr = x(); \
-FORCEINLINE void* x() \
-{ \
-    if (x##Addr) \
-        return x##Addr; \
-    CommonLoader_GetAPI()->SetState(CMN_LOADER_STATE_SKIP_SIG_VALIDATION, 1); \
-    x##Addr = CommonLoader_GetAPI()->ScanSignature(__VA_ARGS__); \
-    printf("[Signature] %s received: 0x%llx\n", #x, x##Addr); \
-    return x##Addr; \
-}
-
-#else
-
 inline MODULEINFO moduleInfo;
 
 inline const MODULEINFO& GetModuleInfo()
@@ -80,10 +48,19 @@ FORCEINLINE void* x() \
 { \
     if (x##Addr) \
         return x##Addr; \
-    x##Addr = ScanSignature(__VA_ARGS__); \
-    printf("[Signature] %s received: 0x%llx\n", #x, x##Addr); \
+    auto* api = CommonLoader_GetAPI(); \
+    if (api != nullptr) \
+    { \
+        api->SetState(CMN_LOADER_STATE_SKIP_SIG_VALIDATION, 1); \
+        x##Addr = api->ScanSignature(__VA_ARGS__); \
+    } \
+    else \
+    { \
+        x##Addr = ScanSignature(__VA_ARGS__); \
+    } \
     if (x##Addr == nullptr) \
         m_IsSigValid = false; \
+    printf("[Signature] %s received: 0x%llx\n", #x, x##Addr); \
     return x##Addr; \
 }
 
@@ -94,9 +71,16 @@ FORCEINLINE void* x() \
 { \
     if (x##Addr) \
         return x##Addr; \
-    x##Addr = ScanSignature(__VA_ARGS__); \
+    auto* api = CommonLoader_GetAPI(); \
+    if (api != nullptr) \
+    { \
+        api->SetState(CMN_LOADER_STATE_SKIP_SIG_VALIDATION, 1); \
+        x##Addr = api->ScanSignature(__VA_ARGS__); \
+    } \
+    else \
+    { \
+        x##Addr = ScanSignature(__VA_ARGS__); \
+    } \
     printf("[Signature] %s received: 0x%llx\n", #x, x##Addr); \
     return x##Addr; \
 }
-
-#endif
