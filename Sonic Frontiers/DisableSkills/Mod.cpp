b7420a58743a87dc5dc7163f6d137d47fd2176d3
Gabriel <34012267+HyperBE32@users.noreply.github.com>
2023-06-25 08:01:12 +0100

    [Disable Skills] Update signatures for v1.30

--- Sonic Frontiers/DisableSkills/Mod.cpp
+++ Sonic Frontiers/DisableSkills/Mod.cpp
@@ -1,16 +1,16 @@
-CL_SCAN_SIGNATURE(m_SigActionProcessor, "\x48\x89\x5C\x24\x08\x48\x89\x74\x24\x10\x48\x89\x7C\x24\x18\x55\x41\x56\x41\x57\x48\x8B\xEC\x48\x83\xEC\x60\x48", "xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-CL_SCAN_SIGNATURE(m_SigSetCurrentStateCaller, "\xE8\xCC\xCC\xCC\xCC\x48\x8D\x4D\xC0\xE8\xCC\xCC\xCC\xCC\x48\x8B\x47\x50", "x????xxxxx????xxxx");
+CL_SCAN_SIGNATURE(m_SigActionProcessor, "\x48\x89\x5C\x24\x08\x48\x89\x74\x24\x10\x48\x89\x7C\x24\x18\x55\x41\x56\x41\x57\x48\x8B\xEC\x48\x83\xEC\x60\x48\x0F\xBE\xF2", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+CL_SCAN_SIGNATURE(m_SigSetCurrentStateCaller, "\xE8\xCC\xCC\xCC\xCC\x48\x8B\x47\x50\xBA\xFF\xF5\xD5\x14\x48\x8B\x88\x80\x00\x00\x00\xE8\xCC\xCC\xCC\xCC\x40\x0F\xB6\xD6", "x????xxxxxxxxxxxxxxxxx????xxxx");
 
-CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisableAirTricksFromTerrain, "\x74\x10\x41\xB0\x01\xBA\x28\x00\x00\x00", "xxxxxxxxxx");
+CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisableAirTricksFromTerrain, "\x74\x19\x41\xB0\x01\x8B\x0D", "xxxxxxx");
 CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisableAirTricksFromTraversalObjects, "\xB0\x01\x88\x46\x3E", "xxxxx");
 CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisableCyloop, "\x0F\x84\xC8\x00\x00\x00\xC0\xE8\x06", "xxxxxxxxx");
 CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisableGrandSlam, "\xE8\xCC\xCC\xCC\xCC\x48\x8B\x5C\x24\x60\x0F\x28\x7C\x24\x20", "x????xxxxxxxxxx");
 CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisablePhantomRush, "\x72\x09\x66\x83\x89\xF0\x00\x00\x00\x40\xC3\x0F\x2F\xC2\x77\x0C\xB8\xBF\xFF\x00\x00\x66\x21\x81\xF0\x00\x00\x00\xC3", "x?xxx????xxxxxx?xxxxxxxx????x");
-CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisablePhantomRushUI, "\x0F\x84\x98\x00\x00\x00\xBA\x1C\x23\x00\x00", "xxxxxxxxxxx");
+CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisablePhantomRushUI, "\x0F\x84\x98\x00\x00\x00\xBA\x4C\x23\x00\x00", "xxxxxxxxxxx");
 CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisableQuickCyloopUI, "\x80\x4F\x38\x01\x48\x8B\xCE", "xxxxxxx");
 CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisableRecoverySmash, "\x0F\x84\xD5\x00\x00\x00\x48\x8B\xD7\x49\x8B\xCD", "xxxxxxxxxxxx");
 CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisableSonicBoom, "\xE8\xCC\xCC\xCC\xCC\xB0\x01\x48\x8B\x5C\x24\x70\x48\x83\xC4\x60\x5F\xC3\x48\x8B\x5C\x24\x70", "x????xxxxxxxxxxxxxxxxxx");
-CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisableSpinSlash, "\x74\x79\xBA\x2D\x00\x00\x00", "xxxxxxx");
+CL_SCAN_SIGNATURE_ALLOW_NULL(m_SigDisableSpinSlash, "\x74\x79\xBA\x2E\x00\x00\x00", "xxxxxxx");
 
 enum EAction : int8_t
 {
@@ -48,7 +48,7 @@ enum EAction : int8_t
 
 FUNCTION_PTR(char, __fastcall, fpSetCurrentState, READ_CALL((int64_t)m_SigSetCurrentStateCaller()), int64_t in_gocPlayerHsmField56, int in_stateIndex, int a3);
 
-HOOK(int64_t, __fastcall, ActionProcessor, m_SigActionProcessor(), int64_t a1, EAction in_action, int8_t a3, int64_t a4)
+HOOK(int64_t, __fastcall, ActionProcessor, m_SigActionProcessor(), int64_t a1, EAction in_action, int8_t a3, int64_t a4, char a5)
 {
 #if _DEBUG
 	printf("[DisableSkills] in_attackId: %d\n", in_action);
@@ -57,18 +57,18 @@ HOOK(int64_t, __fastcall, ActionProcessor, m_SigActionProcessor(), int64_t a1, E
 	switch (in_action)
 	{
 		case EAction_CrossSlash:
-			return Configuration::IsNoCrossSlash ? 0 : originalActionProcessor(a1, in_action, a3, a4);
+			return Configuration::IsNoCrossSlash ? 0 : originalActionProcessor(a1, in_action, a3, a4, a5);
 
 		case EAction_ChargeAttack:
-			return Configuration::IsNoCycloneKick ? 0 : originalActionProcessor(a1, in_action, a3, a4);
+			return Configuration::IsNoCycloneKick ? 0 : originalActionProcessor(a1, in_action, a3, a4, a5);
 
 		case EAction_Smash:
 		{
 			// This ID is shared between Grand Slam and Recovery Smash...
 			if (Configuration::IsNoGrandSlam && !Configuration::IsNoRecoverySmash)
-				return originalActionProcessor(a1, in_action, a3, a4);
+				return originalActionProcessor(a1, in_action, a3, a4, a5);
 
-			return Configuration::IsNoGrandSlam ? 0 : originalActionProcessor(a1, in_action, a3, a4);
+			return Configuration::IsNoGrandSlam ? 0 : originalActionProcessor(a1, in_action, a3, a4, a5);
 		}
 
 		case EAction_HomingShot:
@@ -77,28 +77,28 @@ HOOK(int64_t, __fastcall, ActionProcessor, m_SigActionProcessor(), int64_t a1, E
 				break;
 			
 			// Switch to StateHomingAttack.
-			fpSetCurrentState(a1, 60, 0);
+			fpSetCurrentState(a1, 62, 0);
 
 			return 0;
 		}
 
 		case EAction_LoopKick:
-			return Configuration::IsNoLoopKick ? 0 : originalActionProcessor(a1, in_action, a3, a4);
+			return Configuration::IsNoLoopKick ? 0 : originalActionProcessor(a1, in_action, a3, a4, a5);
 
 		case EAction_QuickCyloop:
-			return Configuration::IsNoQuickCyloop ? 0 : originalActionProcessor(a1, in_action, a3, a4);
+			return Configuration::IsNoQuickCyloop ? 0 : originalActionProcessor(a1, in_action, a3, a4, a5);
 
 		case EAction_SpinSlash:
-			return Configuration::IsNoSpinSlash ? 0 : originalActionProcessor(a1, in_action, a3, a4);
+			return Configuration::IsNoSpinSlash ? 0 : originalActionProcessor(a1, in_action, a3, a4, a5);
 
 		case EAction_Stomping:
-			return Configuration::IsNoStompAttack ? 0 : originalActionProcessor(a1, in_action, a3, a4);
+			return Configuration::IsNoStompAttack ? 0 : originalActionProcessor(a1, in_action, a3, a4, a5);
 
 		case EAction_Crasher:
-			return Configuration::IsNoWildRush ? 0 : originalActionProcessor(a1, in_action, a3, a4);
+			return Configuration::IsNoWildRush ? 0 : originalActionProcessor(a1, in_action, a3, a4, a5);
 	}
 
-	return originalActionProcessor(a1, in_action, a3, a4);
+	return originalActionProcessor(a1, in_action, a3, a4, a5);
 }
 
 EXPORT void Init()
